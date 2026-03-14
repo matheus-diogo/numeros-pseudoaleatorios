a648101a7d6e2456823843cdedd4a648cef6704a
Matheus Costa <matheusjampa1@gmail.com>
2026-03-14 11:46:37 -0300

    testand

--- simulação.R
+++ simulação.R
@@ -1,1 +1,4 @@
-# Código da Simução de Geração de Números Pseudosaletórios
+b <- 1:6
+d <- c(sqrt(2),sqrt(4),sqrt(6))
+resultado <- sum(b+d)
+if(resultado > 45) {print("Que mentira")} else {print("Hello world")}
